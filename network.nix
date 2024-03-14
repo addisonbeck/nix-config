aaff29565cedec33d36b5878555d424d01934298
Addison Beck <hello@addisonbeck.com>
2024-03-13 19:27:47 -0500

    Adjust network to specify specific hostname for DO

--- network.nix
+++ network.nix
@@ -1,6 +1,10 @@
-{ modulesPath, lib, ... }:
 {
-  imports = lib.optional (builtins.pathExists ./do-userdata.nix) ./do-userdata.nix ++ [
-    (modulesPath + "/virtualisation/digital-ocean-config.nix")
-  ];
+  nix.addisonbeck.dev = { modulesPath, lib, ... }: {
+    imports = lib.optional (builtins.pathExists ./do-userdata.nix) ./do-userdata.nix ++ [
+      (modulesPath + "/virtualisation/digital-ocean-config.nix")
+    ];
+
+    deployment.targetHost = "159.65.168.64";
+    deployment.targetUser = "root";
+  };
 }
