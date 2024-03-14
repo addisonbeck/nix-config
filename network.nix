f357c927c661e44aca4327d89606ff71c73e7d0b
Addison Beck <hello@addisonbeck.com>
2024-03-13 19:29:23 -0500

    Explicitly add name

--- network.nix
+++ network.nix
@@ -1,10 +1,11 @@
 {
-  nix.addisonbeck.dev = { modulesPath, lib, ... }: {
+  nix.addisonbeck.dev = { modulesPath, lib, name, ... }: {
     imports = lib.optional (builtins.pathExists ./do-userdata.nix) ./do-userdata.nix ++ [
       (modulesPath + "/virtualisation/digital-ocean-config.nix")
     ];
 
     deployment.targetHost = "159.65.168.64";
     deployment.targetUser = "root";
+    networking.hostName = name;
   };
 }
